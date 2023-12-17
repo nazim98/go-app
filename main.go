60902020f0a6827b905fe5d969775158ce028498
nazim98 <t.nazim1998@gmail.com>
2023-12-17 11:36:56 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	var b = " Value"
+	fmt.Println(a + b)
+
+	if 7+3 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+}
